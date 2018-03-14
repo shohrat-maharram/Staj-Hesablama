7e7173461ba434ddd9e87c00600a6a110b64bade
Shohrat Maharram <shohret550@gmail.com>
2018-03-14 17:42:32 +0400

    Application-ın təxminən 20%-i hazırdır.

---
+++ Staj/Staj/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace Staj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpGirme1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpCixma1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNetice = new System.Windows.Forms.Label();
+            this.btnHesabla = new System.Windows.Forms.Button();
+            this.btnElaveEt = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İşə qəbul tarixi";
+            // 
+            // dtpGirme1
+            // 
+            this.dtpGirme1.CustomFormat = "dd.MM.yyyy";
+            this.dtpGirme1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpGirme1.Location = new System.Drawing.Point(32, 34);
+            this.dtpGirme1.Name = "dtpGirme1";
+            this.dtpGirme1.Size = new System.Drawing.Size(147, 20);
+            this.dtpGirme1.TabIndex = 1;
+            // 
+            // dtpCixma1
+            // 
+            this.dtpCixma1.CustomFormat = "dd.MM.yyyy";
+            this.dtpCixma1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpCixma1.Location = new System.Drawing.Point(205, 34);
+            this.dtpCixma1.Name = "dtpCixma1";
+            this.dtpCixma1.Size = new System.Drawing.Size(147, 20);
+            this.dtpCixma1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "İşdən çıxma tarixi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cəm iş stajı: ";
+            // 
+            // lblNetice
+            // 
+            this.lblNetice.AutoSize = true;
+            this.lblNetice.Location = new System.Drawing.Point(236, 366);
+            this.lblNetice.Name = "lblNetice";
+            this.lblNetice.Size = new System.Drawing.Size(13, 13);
+            this.lblNetice.TabIndex = 5;
+            this.lblNetice.Text = "0";
+            // 
+            // btnHesabla
+            // 
+            this.btnHesabla.Location = new System.Drawing.Point(32, 356);
+            this.btnHesabla.Name = "btnHesabla";
+            this.btnHesabla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesabla.TabIndex = 6;
+            this.btnHesabla.Text = "Hesabla";
+            this.btnHesabla.UseVisualStyleBackColor = true;
+            this.btnHesabla.Click += new System.EventHandler(this.btnHesabla_Click);
+            // 
+            // btnElaveEt
+            // 
+            this.btnElaveEt.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnElaveEt.Location = new System.Drawing.Point(358, 33);
+            this.btnElaveEt.Name = "btnElaveEt";
+            this.btnElaveEt.Size = new System.Drawing.Size(69, 22);
+            this.btnElaveEt.TabIndex = 7;
+            this.btnElaveEt.Text = "Əlavə et";
+            this.btnElaveEt.UseVisualStyleBackColor = false;
+            this.btnElaveEt.Click += new System.EventHandler(this.btnElaveEt_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(13, 38);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(13, 13);
+            this.lbl1.TabIndex = 9;
+            this.lbl1.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 407);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnElaveEt);
+            this.Controls.Add(this.btnHesabla);
+            this.Controls.Add(this.lblNetice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpCixma1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpGirme1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Staj Hesablama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpGirme1;
+        private System.Windows.Forms.DateTimePicker dtpCixma1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNetice;
+        private System.Windows.Forms.Button btnHesabla;
+        private System.Windows.Forms.Button btnElaveEt;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ Staj/Staj/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Staj
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public List<DateTimePicker> dtps = new List<DateTimePicker>();
+
+        private void btnHesabla_Click(object sender, EventArgs e)
+        {
+            DateTime dtpGirme2 =Convert.ToDateTime(this.Controls["dtpGirme2"].Text);
+            DateTime dtpCixma2 = Convert.ToDateTime(this.Controls["dtpCixma2"].Text);
+            TimeSpan staj2 = dtpCixma2.Date.Subtract(dtpGirme2.Date);
+
+            TimeSpan staj1 = dtpCixma1.Value.Subtract(dtpGirme1.Value);
+            int hours = staj1.Days*24;
+
+            int year = Convert.ToInt32(hours / (365*24));
+
+            int month = Convert.ToInt32(Math.Floor((hours-year*8760)/729.999));
+
+            int days = Convert.ToInt32((hours - (year * 8760)-(month*729.999))/24);
+
+            MessageBox.Show(staj2.ToString());
+            lblNetice.Text = (year + " il: " + month + " ay: " + days + " gün (±3 gün)");
+        }
+
+        public int i = 2;
+        public int a = 64;
+        public int b = 68;
+
+        private void btnElaveEt_Click(object sender, EventArgs e)
+        {
+            if (i<=10)
+            {
+                addNewdtp1();
+                addNewdtp2();
+                addNewlbl1();
+                btnElaveEt.Top = a;
+
+                i++;
+                a = a + 30;
+                b = b + 30;
+                
+            }            
+        }
+
+        public System.Windows.Forms.Label addNewlbl1()
+        {
+            System.Windows.Forms.Label lbl = new System.Windows.Forms.Label();
+            this.Controls.Add(lbl);
+            lbl.Top = b;
+            lbl.Left = 13;
+            lbl.Width = 147;
+            lbl.Height = 20;
+            lbl.Name = "lbl" + i;
+            lbl.Text = i.ToString();
+
+            return lbl;
+        }
+
+        public System.Windows.Forms.DateTimePicker addNewdtp1()
+        {
+            System.Windows.Forms.DateTimePicker dtp = new System.Windows.Forms.DateTimePicker();
+            this.Controls.Add(dtp);
+            dtp.CustomFormat = "dd.MM.yyyy";
+            dtp.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            dtp.Top = a;
+            dtp.Left = 32;
+            dtp.Width = 147;
+            dtp.Height = 20;
+            dtp.Name = "dtpGirme" + i;
+            //dtps.Add(Convert.ToDateTime(dtp.Name));
+            return dtp;
+        }
+
+        public System.Windows.Forms.DateTimePicker addNewdtp2()
+        {
+            System.Windows.Forms.DateTimePicker dtp = new System.Windows.Forms.DateTimePicker();
+            this.Controls.Add(dtp);
+            dtp.CustomFormat = "dd.MM.yyyy";
+            dtp.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            dtp.Top = a;
+            dtp.Left = 205;
+            dtp.Width = 147;
+            dtp.Height = 20;
+            dtp.Name = "dtpCixma" + i;
+            return dtp;
+        }
+    }
+}
